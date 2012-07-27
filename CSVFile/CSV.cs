69753a3e438f6f59acb72e138e6e780a4a3aedc0
glassware1975@gmail.com <glassware1975@gmail.com@1df7587c-9477-9a6c-5200-2f0a3c2662b7>
2012-07-27 05:00:14 +0000

    Imported first draft of code

---
+++ CSVFile/CSV.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+
+namespace CSVFile
+{
+    public static class CSV
+    {
+        public const char DEFAULT_DELIMITER = ',';
+        public const char DEFAULT_QUALIFIER = '"';
+        public const char DEFAULT_TAB_DELIMITER = '\t';
+
+        #region Reading CSV Formatted Data
+        /// <summary>
+        /// Parse a line whose values may include newline symbols or CR/LF
+        /// </summary>
+        /// <param name="sr"></param>
+        /// <returns></returns>
+        public static string[] ParseMultiLine(StreamReader sr, char delimiter = DEFAULT_DELIMITER, char text_qualifier = DEFAULT_QUALIFIER)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] array = null;
+            while (!sr.EndOfStream) {
+
+                // Read in a line
+                sb.Append(sr.ReadLine());
+
+                // Does it parse?
+                string s = sb.ToString();
+                if (TryParseLine(s, delimiter, text_qualifier, out array)) {
+                    return array;
+                }
+
+                // We didn't succeed on the first try - our line must have an embedded newline in it
+                sb.Append("\n");
+            }
+
+            // Fails to parse - return the best array we were able to get
+            return array;
+        }
+
+        /// <summary>
+        /// Parse the line and return the array if it succeeds, or as best as we can get
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="delimiter"></param>
+        /// <param name="text_qualifier"></param>
+        /// <returns></returns>
+        public static string[] ParseLine(string s, char delimiter = DEFAULT_DELIMITER, char text_qualifier = DEFAULT_QUALIFIER)
+        {
+            string[] array = null;
+            TryParseLine(s, delimiter, text_qualifier, out array);
+            return array;
+        }
+
+        /// <summary>
+        /// Read in a line of text, and use the Add() function to add these items to the current CSV structure
+        /// </summary>
+        /// <param name="s"></param>
+        public static bool TryParseLine(string s, char delimiter, char text_qualifier, out string[] array)
+        {
+            bool success = true;
+            List<string> list = new List<string>();
+            StringBuilder work = new StringBuilder();
+            for (int i = 0; i < s.Length; i++) {
+                char c = s[i];
+
+                // If we are starting a new field, is this field text qualified?
+                if ((c == text_qualifier) && (work.Length == 0)) {
+                    int p2;
+                    while (true) {
+                        p2 = s.IndexOf(text_qualifier, i + 1);
+
+                        // for some reason, this text qualifier is broken
+                        if (p2 < 0) {
+                            work.Append(s.Substring(i + 1));
+                            i = s.Length;
+                            success = false;
+                            break;
+                        }
+
+                        // Append this qualified string
+                        work.Append(s.Substring(i + 1, p2 - i - 1));
+                        i = p2;
+
+                        // If this is a double quote, keep going!
+                        if (((p2 + 1) < s.Length) && (s[p2 + 1] == text_qualifier)) {
+                            work.Append(text_qualifier);
+                            i++;
+
+                            // otherwise, this is a single qualifier, we're done
+                        } else {
+                            break;
+                        }
+                    }
+
+                    // Does this start a new field?
+                } else if (c == delimiter) {
+                    list.Add(work.ToString());
+                    work.Length = 0;
+
+                    // Test for special case: when the user has written a casual comma, space, and text qualifier, skip the space
+                    // Checks if the second parameter of the if statement will pass through successfully
+                    // e.g. "bob", "mary", "bill"
+                    if (i + 2 <= s.Length - 1) {
+                        if (s[i + 1].Equals(' ') && s[i + 2].Equals(text_qualifier)) {
+                            i++;
+                        }
+                    }
+                } else {
+                    work.Append(c);
+                }
+            }
+            list.Add(work.ToString());
+
+            // If we have nothing in the list, and it's possible that this might be a tab delimited list, try that before giving up
+            if (list.Count == 1 && delimiter != DEFAULT_TAB_DELIMITER) {
+                string[] tab_delimited_array = ParseLine(s, DEFAULT_TAB_DELIMITER, DEFAULT_QUALIFIER);
+                if (tab_delimited_array.Length > list.Count) {
+                    array = tab_delimited_array;
+                    return success;
+                }
+            }
+
+            // Return the array we parsed
+            array = list.ToArray();
+            return success;
+        }
+        #endregion
+
+        #region Output CSV formatted files from raw data
+        /// <summary>
+        /// Write a data table to disk at the designated file name in CSV format
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="fn"></param>
+        public static void SaveAsCSV(this DataTable dt, string filename, bool save_column_names, char delim = DEFAULT_DELIMITER, char qual = DEFAULT_QUALIFIER)
+        {
+            using (StreamWriter sw = new StreamWriter(filename)) {
+
+                // Write headers, if the caller requested we do so
+                if (save_column_names) {
+                    List<string> headers = new List<string>();
+                    foreach (DataColumn col in dt.Columns) {
+                        headers.Add(col.ColumnName);
+                    }
+                    sw.WriteLine(Output(headers, delim, qual));
+                }
+
+                // Now produce the rows
+                foreach (DataRow dr in dt.Rows) {
+                    sw.WriteLine(Output(dr.ItemArray, delim, qual));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Output a single field value as appropriate
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string Output(IEnumerable<object> line, char delimiter, char qualifier)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (object o in line) {
+
+                // Null strings are just a delimiter
+                if (o != null) {
+                    string s = o.ToString();
+                    if (s.Length > 0) {
+
+                        // Does this string contain any risky characters?  Risky is defined as delim, qual, or newline
+                        if (s.Contains(delimiter) || s.Contains(qualifier) || s.Contains(Environment.NewLine)) {
+                            sb.Append(qualifier);
+
+                            // Double up any qualifiers that may occur
+                            sb.Append(s.Replace(qualifier.ToString(), qualifier.ToString() + qualifier.ToString()));
+                            sb.Append(qualifier);
+                        } else {
+                            sb.Append(s);
+                        }
+                    }
+                }
+
+                // Move to the next cell
+                sb.Append(delimiter);
+            }
+
+            // Subtract the trailing delimiter so we don't inadvertently add a column
+            sb.Length -= 1;
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ CSVTestSuite/BasicParseTests.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSVFile;
+
+namespace CSVTestSuite
+{
+    [TestClass]
+    public class BasicParseTests
+    {
+        [TestMethod]
+        public void ParseBasicCSV()
+        {
+            // Simplest test
+            string[] line = CSV.ParseLine("1,2,3,4,5");
+            Assert.AreEqual(line.Length, 5);
+            Assert.AreEqual(line[2], "3");
+            Assert.AreEqual(line[4], "5");
+
+            // Test with a trailing blank item
+            line = CSV.ParseLine("1,2,3,4,5,");
+            Assert.AreEqual(line.Length, 6);
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[5], "");
+        }
+
+        [TestMethod]
+        public void ParseTSV()
+        {
+            // Basic TSV test
+            string[] line = CSV.ParseLine("1\t2\t3\t4\t5", '\t', '\"');
+            Assert.AreEqual(line.Length, 5);
+            Assert.AreEqual(line[3], "4");
+            Assert.AreEqual(line[1], "2");
+
+            // Test trailing blank item
+            line = CSV.ParseLine("1\t2\t3\t4\t5\t", '\t', '\"');
+            Assert.AreEqual(line.Length, 6);
+            Assert.AreEqual(line[5], "");
+            Assert.AreEqual(line[4], "5");
+        }
+
+        [TestMethod]
+        public void ParseTextQualifiedCSV()
+        {
+            string[] line = null;
+
+            line = CSV.ParseLine("1,\"two\",3,\"four\",five");
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], "3");
+            Assert.AreEqual(line[3], "four");
+            Assert.AreEqual(line[4], "five");
+            line = CSV.ParseLine("1,\"two\",3,\"four five\",six");
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], "3");
+            Assert.AreEqual(line[3], "four five");
+            Assert.AreEqual(line[4], "six");
+            line = CSV.ParseLine("1,\"two\",3,\"four five 6\",six");
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], "3");
+            Assert.AreEqual(line[3], "four five 6");
+            Assert.AreEqual(line[4], "six");
+
+            // Test with embedded delimiters
+            line = CSV.ParseLine("1,\"two\",3,\"four, five, and 6\",six");
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], "3");
+            Assert.AreEqual(line[3], "four, five, and 6");
+            Assert.AreEqual(line[4], "six");
+
+            // Test with doubled up qualifiers
+            line = CSV.ParseLine("1,\"two\",3,\"four, \"\"five\"\", and 6\",six");
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], "3");
+            Assert.AreEqual(line[3], "four, \"five\", and 6");
+            Assert.AreEqual(line[4], "six");
+
+            // Test with an embedded newline
+            line = CSV.ParseLine("1,\"two\",3,\"four, \"\"five\n\"\", and 6\",six");
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], "3");
+            Assert.AreEqual(line[3], "four, \"five\n\", and 6");
+            Assert.AreEqual(line[4], "six");
+        }
+
+        [TestMethod]
+        public void TestFailingCSVLine()
+        {
+            string[] line = null;
+
+            // Use a basic parse that will do its best but fail to recognize the problem
+            line = CSV.ParseLine("1,\"two\",3,\"four, \"\"five");
+            Assert.AreEqual(line.Length, 4);
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], "3");
+            Assert.AreEqual(line[3], "four, \"five");
+
+            // Confirm that the more advanced parse will fail
+            Assert.IsFalse(CSV.TryParseLine("1,\"two\",3,\"four, \"\"five", CSV.DEFAULT_DELIMITER, CSV.DEFAULT_QUALIFIER, out line));
+        }
+
+        /// <summary>
+        /// This test resolves a rare case that occurs with hand-written CSV files.  sometimes users hand typing the file use
+        /// "english grammar" and include a space after the comma.  This can trip up algorithms that expect the text qualifier
+        /// to be the first character after a delimiter.
+        /// </summary>
+        [TestMethod]
+        public void ParseTextQualifiedCSVWithSpacing()
+        {
+            string[] line = null;
+
+            line = CSV.ParseLine("1, \"two\", 3, \"four\", five");
+            Assert.AreEqual(line.Length, 5);
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], " 3");
+            Assert.AreEqual(line[3], "four");
+            Assert.AreEqual(line[4], " five");
+            line = CSV.ParseLine("1, \"two\", 3, \"four five\", six");
+            Assert.AreEqual(line.Length, 5);
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], " 3");
+            Assert.AreEqual(line[3], "four five");
+            Assert.AreEqual(line[4], " six"); 
+            line = CSV.ParseLine("1, \"two\", 3, \"four five 6\", six");
+            Assert.AreEqual(line.Length, 5);
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], " 3");
+            Assert.AreEqual(line[3], "four five 6");
+            Assert.AreEqual(line[4], " six");
+            line = CSV.ParseLine("1, \"two\", 3, \"four, five, and 6\", six");
+            Assert.AreEqual(line.Length, 5);
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], " 3");
+            Assert.AreEqual(line[3], "four, five, and 6");
+            Assert.AreEqual(line[4], " six");
+            line = CSV.ParseLine("1, \"two\", 3, \"four, \"\"five\"\", \nand 6\", six");
+            Assert.AreEqual(line.Length, 5);
+            Assert.AreEqual(line[0], "1");
+            Assert.AreEqual(line[1], "two");
+            Assert.AreEqual(line[2], " 3");
+            Assert.AreEqual(line[3], "four, \"five\", \nand 6");
+            Assert.AreEqual(line[4], " six");
+        }
+    }
+}
