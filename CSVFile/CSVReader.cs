0527753df67adeeb0b89901b9e5c652b1577f15e
glassware1975@gmail.com <glassware1975@gmail.com@1df7587c-9477-9a6c-5200-2f0a3c2662b7>
2012-07-27 05:04:21 +0000

    Cleaned up the original import

--- CSVFile/CSVReader.cs
+++ CSVFile/CSVReader.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * 2006 - 2012 Ted Spence, http://tedspence.com
+ * License: http://www.apache.org/licenses/LICENSE-2.0 
+ * Home page: https://code.google.com/p/csharp-csv-reader/
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
