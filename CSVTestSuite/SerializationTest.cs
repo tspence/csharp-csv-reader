52f09272464ef1dd9c3d68c1c40fd499f5b13ceb
glassware1975@gmail.com <glassware1975@gmail.com@1df7587c-9477-9a6c-5200-2f0a3c2662b7>
2012-08-17 18:46:46 +0000

    Added new ability to deserialize a CSV file to objects

---
+++ CSVTestSuite/SerializationTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSVFile;
+using System.IO;
+
+namespace CSVTestSuite
+{
+    [TestClass]
+    public class SerializationTest
+    {
+        public class SerializeTest
+        {
+            public string TestString;
+            public string PropertyString { get; set; }
+            public string Comment;
+            public void SetComment(string s) { Comment = s;  }
+            public int IntField;
+            public int IntProperty { get; set; }
+            public DateTime timestamp;
+        }
+
+        [TestMethod]
+        public void TestObjectSerialization()
+        {
+            // Deserialize an array to a list of objects!
+            List<SerializeTest> list = null;
+            string source = @"timestamp,TestString,SetComment,PropertyString,IntField,IntProperty
+2012-05-01,test1,""Hi there, I said!"",Bob,57,0
+2011-04-01,test2,""What's up, buttercup?"",Ralph,1,-999
+1975-06-03,test3,""Bye and bye, dragonfly!"",Jimmy's The Bomb,12,13";
+            byte[] byteArray = Encoding.ASCII.GetBytes(source);
+            MemoryStream stream = new MemoryStream(byteArray);
+            using (CSVReader cr = new CSVReader(new StreamReader(stream))) {
+                list = cr.Deserialize<SerializeTest>();
+            }
+
+            // Test the array
+            Assert.AreEqual(list.Count, 3);
+            Assert.AreEqual(list[0].TestString, "test1");
+            Assert.AreEqual(list[0].Comment, "Hi there, I said!");
+            Assert.AreEqual(list[0].PropertyString, "Bob");
+            Assert.AreEqual(list[0].IntField, 57);
+            Assert.AreEqual(list[0].IntProperty, 0);
+            Assert.AreEqual(list[0].timestamp, new DateTime(2012,5,1));
+            Assert.AreEqual(list[1].TestString, "test2");
+            Assert.AreEqual(list[1].Comment, "What's up, buttercup?");
+            Assert.AreEqual(list[1].PropertyString, "Ralph");
+            Assert.AreEqual(list[1].IntField, 1);
+            Assert.AreEqual(list[1].IntProperty, -999);
+            Assert.AreEqual(list[1].timestamp, new DateTime(2011, 4, 1));
+            Assert.AreEqual(list[2].TestString, "test3");
+            Assert.AreEqual(list[2].Comment, "Bye and bye, dragonfly!");
+            Assert.AreEqual(list[2].PropertyString, "Jimmy's The Bomb");
+            Assert.AreEqual(list[2].IntField, 12);
+            Assert.AreEqual(list[2].IntProperty, 13);
+            Assert.AreEqual(list[2].timestamp, new DateTime(1975, 6, 3));
+        }
+    }
+}
