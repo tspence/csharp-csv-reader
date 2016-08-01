f3d36cb5502aeb3395b178b71d8eb5a46309f687
Ted Spence <Ted Spence>
2016-07-31 21:39:41 -0700

    Updated licensing and published to Nuget

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CSVFile")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple library for processing CSV files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ted Spence")]
 [assembly: AssemblyProduct("CSVFile")]
-[assembly: AssemblyCopyright("Copyright (C) 2012-2015 Ted Spence")]
+[assembly: AssemblyCopyright("Copyright (C) 2006 - 2016 Ted Spence")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
