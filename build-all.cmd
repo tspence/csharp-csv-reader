d6454b2ad32e19f27f940531adee5b048c32de46
ted-spence-avalara <ted.spence@avalara.com>
2016-11-03 16:48:15 -0700

    Whoah it all builds now into separate folders

--- build-all.cmd
+++ build-all.cmd
@@ -1,8 +1,11 @@
 @ECHO OFF
 ECHO ********** Building all "older .net" variants
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net20.csproj /property:Config=Debug+Release
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net35.csproj /property:Config=Debug+Release
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net40.csproj /property:Config=Debug+Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net20.csproj /property:Configuration=Debug
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net20.csproj /property:Configuration=Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net35.csproj /property:Configuration=Debug
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net35.csproj /property:Configuration=Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net40.csproj /property:Configuration=Debug
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net40.csproj /property:Configuration=Release
 
 ECHO ********** Building "dotnetcore" variants
 pushd .
