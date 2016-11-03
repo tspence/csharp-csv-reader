ea7d865592ec527fdc408a058d2a14bb0bfa8a6a
ted-spence-avalara <ted.spence@avalara.com>
2016-11-03 16:41:25 -0700

    Somehow it compiles

--- build-all.cmd
+++ build-all.cmd
@@ -5,10 +5,11 @@ C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reade
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.net40.csproj /property:Config=Debug+Release
 
 ECHO ********** Building "dotnetcore" variants
-pushd .
-cd src
-dotnet.exe build csharp-csv-reader.portable.csproj -c Debug
-dotnet.exe build csharp-csv-reader.portable.csproj -c Release
-popd
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe src\csharp-csv-reader.portable.csproj /property:Config=Debug+Release
+REM pushd .
+REM cd src
+REM dotnet.exe build csharp-csv-reader.portable.csproj -c Debug
+REM dotnet.exe build csharp-csv-reader.portable.csproj -c Release
+REM popd
 
 ECHO ********** Done
