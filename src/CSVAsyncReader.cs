7e4ef400205d35daebd17ae88f52754ef4e4cd55
Ted Spence <tspence@lockstep.io>
2022-02-02 09:21:52 -0800

    Cleanup async reader
    The async reader file is only included fror NET50 projects, so there's no need to use #if statements around it.  Let's also follow a few compiler warnings/suggestions.

--- src/CSVAsyncReader.cs
+++ src/CSVAsyncReader.cs
@@ -5,11 +5,7 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
-#if HAS_DATATABLE
-using System.Data;
-#endif
 using System.Reflection;
 using System.ComponentModel;
 using System.Threading.Tasks;
@@ -17,17 +13,16 @@ using System.Threading;
 
 namespace CSVFile
 {
-#if NET50
     public class CSVAsyncReader : IAsyncEnumerable<string[]>, IDisposable
     {
-        protected CSVSettings _settings;
-        protected IAsyncEnumerable<string[]> _enumerable;
-        protected StreamReader _instream;
+        private readonly CSVSettings _settings;
+        private IAsyncEnumerable<string[]> _enumerable;
+        private readonly StreamReader _stream;
 
         /// <summary>
         /// If the first row in the file is a header row, this will be populated
         /// </summary>
-        public string[] Headers = null;
+        public string[] Headers;
 
         /// <summary>
         /// Construct a new CSV reader off a streamed source
@@ -36,7 +31,7 @@ namespace CSVFile
         /// <param name="settings">The CSV settings to use for this reader (Default: CSV)</param>
         private CSVAsyncReader(StreamReader source, CSVSettings settings = null)
         {
-            _instream = source;
+            _stream = source;
             _settings = settings ?? CSVSettings.CSV;
             _enumerable = null;
         }
@@ -44,7 +39,7 @@ namespace CSVFile
         public static async Task<CSVAsyncReader> From(StreamReader source, CSVSettings settings = null)
         {
             var reader = new CSVAsyncReader(source, settings);
-            reader._enumerable = CSV.ParseStreamAsync(reader._instream, reader._settings);
+            reader._enumerable = CSV.ParseStreamAsync(reader._stream, reader._settings);
 
             // Do we need to parse the first line of the file as headers?
             if (reader._settings.HeaderRowIncluded)
@@ -76,7 +71,7 @@ namespace CSVFile
         /// </summary>
         public void Dispose()
         {
-            _instream.Dispose();
+            _stream.Dispose();
         }
 
         public IAsyncEnumerator<string[]> GetAsyncEnumerator(CancellationToken cancellationToken = default)
@@ -90,20 +85,20 @@ namespace CSVFile
         /// </summary>
         public async Task<List<T>> Deserialize<T>() where T : class, new()
         {
-            List<T> result = new List<T>();
-            Type return_type = typeof(T);
+            var result = new List<T>();
+            var return_type = typeof(T);
 
             // Read in the first line - we have to have headers!
             if (Headers == null) throw new Exception("CSV must have headers to be deserialized");
-            int num_columns = Headers.Length;
+            var num_columns = Headers.Length;
 
             // Determine how to handle each column in the file - check properties, fields, and methods
-            Type[] column_types = new Type[num_columns];
-            TypeConverter[] column_convert = new TypeConverter[num_columns];
-            PropertyInfo[] prop_handlers = new PropertyInfo[num_columns];
-            FieldInfo[] field_handlers = new FieldInfo[num_columns];
-            MethodInfo[] method_handlers = new MethodInfo[num_columns];
-            for (int i = 0; i < num_columns; i++)
+            var column_types = new Type[num_columns];
+            var column_convert = new TypeConverter[num_columns];
+            var prop_handlers = new PropertyInfo[num_columns];
+            var field_handlers = new FieldInfo[num_columns];
+            var method_handlers = new MethodInfo[num_columns];
+            for (var i = 0; i < num_columns; i++)
             {
                 prop_handlers[i] = return_type.GetProperty(Headers[i]);
 
@@ -117,7 +112,7 @@ namespace CSVFile
                     {
 
                         // Methods must be treated differently - we have to ensure that the method has a single parameter
-                        MethodInfo mi = return_type.GetMethod(Headers[i]);
+                        var mi = return_type.GetMethod(Headers[i]);
                         if (mi != null)
                         {
                             if (mi.GetParameters().Length == 1)
@@ -159,8 +154,8 @@ namespace CSVFile
             }
 
             // Alright, let's retrieve CSV lines and parse each one!
-            int row_num = 1;
-            await foreach (string[] line in this)
+            var row_num = 1;
+            await foreach (var line in this)
             {
 
                 // Does this line match the length of the first line?  Does the caller want us to complain?
@@ -170,25 +165,21 @@ namespace CSVFile
                 }
 
                 // Construct a new object and execute each column on it
-                T obj = new T();
-                for (int i = 0; i < Math.Min(line.Length, num_columns); i++)
+                var obj = new T();
+                for (var i = 0; i < Math.Min(line.Length, num_columns); i++)
                 {
 
                     // Attempt to convert this to the specified type
                     object value = null;
-                    if (_settings.AllowNull && (line[i] == null))
-                    {
-                        value = null;
-                    }
-                    else if (column_convert[i] != null && column_convert[i].IsValid(line[i]))
+                    if (column_convert[i] != null && column_convert[i].IsValid(line[i]))
                     {
                         value = column_convert[i].ConvertFromString(line[i]);
                     }
                     else if (!_settings.IgnoreHeaderErrors)
                     {
-                        throw new Exception(String.Format("The value '{0}' cannot be converted to the type {1}.", line[i], column_types[i]));
+                        throw new Exception($"The value '{line[i]}' cannot be converted to the type {column_types[i]}.");
                     }
-
+                     
                     // Can we set this value to the object as a property?
                     if (prop_handlers[i] != null)
                     {
@@ -204,7 +195,7 @@ namespace CSVFile
                     }
                     else if (method_handlers[i] != null)
                     {
-                        method_handlers[i].Invoke(obj, new object[] { value });
+                        method_handlers[i].Invoke(obj, new[] { value });
                     }
                 }
 
@@ -217,5 +208,4 @@ namespace CSVFile
             return result;
         }
     }
-#endif
 }
