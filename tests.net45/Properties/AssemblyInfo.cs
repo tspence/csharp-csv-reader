23ca5be6d83e04b2034f85b934923e8d4caf3b9f
Ted Spence <ted@spence.net>
2022-12-09 23:22:00 -0800

    Upgrade all projects to SDK style (#51)
    * Migrating projects to SDK style
    * Split off compatibility projects from modern projects
    * Remove SmtpClient code

--- tests.net45/Properties/AssemblyInfo.cs
+++ tests.net45/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSVFile")]
-[assembly: AssemblyDescription("A minimal CSV and TSV parsing library with minimal dependencies.  Compatible with most dot net versions.")]
+[assembly: AssemblyTitle("tests.net45")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ted Spence")]
-[assembly: AssemblyProduct("CSVFile")]
-[assembly: AssemblyCopyright("Copyright (C) 2006 - 2021 Ted Spence")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tests.net45")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8eabdb7f-0c62-42cb-8876-1ea03cfc48e9")]
+[assembly: Guid("f72ec540-8005-403e-9dd0-e0c5c39df10e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.2.0")]
-[assembly: AssemblyFileVersion("3.0.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
